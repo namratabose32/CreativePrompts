ae8f97221d21f73f469a72dff0ac31781adad842
Namrata <namratabose32@gmail.com>
2023-07-10 03:46:22 +0530

    Authenticated signIn session by next-auth & connected to mongoDB

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,27 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
-export const metadata ={
-    title:"Promptopia",
-    description:'Discover & Share AI Prompts'
-}
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-            <main className='app'>
-                <Nav/>
-                {children}
-            </main>
-        </body>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
